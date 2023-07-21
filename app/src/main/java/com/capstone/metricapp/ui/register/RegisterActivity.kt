dc949897b1c6c896ea6babfb849c8a1378bca10d
Krisna <krisnaerlangga08@gmail.com>
2023-07-21 22:55:34 +0700

    add: setup splash, login, register UI :rocket:

---
+++ app/src/main/java/com/capstone/metricapp/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.metricapp.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.metricapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
