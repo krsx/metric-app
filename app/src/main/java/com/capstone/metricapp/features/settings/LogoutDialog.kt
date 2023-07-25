c15b1bdfbd3964da2e1a94742d3f7673cc0c2752
Krisna <krisnaerlangga08@gmail.com>
2023-07-25 10:54:16 +0700

    add: logout dialog :rocket:

--- app/src/main/java/com/capstone/metricapp/features/settings/LogoutDialog.kt
+++ app/src/main/java/com/capstone/metricapp/features/settings/LogoutDialog.kt
@@ -1,8 +1,9 @@
 package com.capstone.metricapp.features.settings
 
+import android.app.Dialog
+import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.capstone.metricapp.R
 
 class LogoutDialog: DialogFragment() {
-
-
 }
