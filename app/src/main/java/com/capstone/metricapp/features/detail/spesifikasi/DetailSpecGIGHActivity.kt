492def1b10f83248db1e2f6801fbdf8e3ee579be
Krisna <krisnaerlangga08@gmail.com>
2023-07-26 21:46:58 +0700

    add: add spec and header layout :rocket:

---
+++ app/src/main/java/com/capstone/metricapp/features/detail/spesifikasi/DetailSpecGIGHActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.metricapp.features.detail.spesifikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.metricapp.R
+
+class DetailSpecGIGHActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_spec_gighactivity)
+    }
+}
