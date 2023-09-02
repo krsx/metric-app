b290e7e6d45a3c5847f645a206898c5f58d8e280
Krisna <krisnaerlangga08@gmail.com>
2023-09-02 23:21:38 +0700

    refactor: keypoint history for all history keypoint respone :hammer:

--- app/src/main/java/com/capstone/metricapp/core/domain/model/KeypointHistory.kt
+++ app/src/main/java/com/capstone/metricapp/core/domain/model/KeypointHistory.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class ScadatelHistory(
+data class KeypointHistory(
     var id: String,
     var documentId: String,
     var modelName: String,
